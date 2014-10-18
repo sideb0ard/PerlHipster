17d26d245f193e0ce5d1412f85eceb121deb180c
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2014-10-17 23:54:57 -0700

    its tha innit

---
+++ mergsort.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+use strict;
+use POSIX;
+
+my @cleannumz = grep(/^\d+$/, @ARGV);
+print "\n::::\nSorting @cleannumz...\n\n";
+
+if ((scalar @cleannumz) > 1) {
+  my @results = splitzort(@cleannumz);
+  print "REZULTS ARE IN: @results\n\n";
+}
+
+sub splitzort {
+  my @numz = @_;
+  if ((scalar @numz) <= 1) {
+    return @numz;
+  }
+  my @leftarray = splice @numz, 0, floor((scalar @numz) /2);
+  # recursively call the split
+  my @left = splitzort(@leftarray);
+  my @right = splitzort(@numz);
+  # * conquer part!
+  return merge(\@left,\@right);
+}
+
+sub merge (\@\@) {
+  my @results;
+  my ($left, $right) = @_;
+  my @left = @{$left};
+  my @right = @{$right};
+  while ((scalar @left) > 0 || (scalar @right) > 0) {
+    if ((scalar @left) > 0 && (scalar @right) > 0) {
+      if (($left[0]) <= ($right[0])) {
+        push @results, shift @left;
+      } else {
+        push @results, shift @right;
+      }
+    } elsif ((scalar @left) > 0 ) {
+      push @results, shift @left;
+    } else {
+      push @results, shift @right;
+    }
+  }
+  return @results;
+}
